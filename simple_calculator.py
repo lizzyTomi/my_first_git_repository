5a61901fa711826492ca1b68eb3eecac56b4c524
lawrence emmanuel <lizzytomi17@gmail.com>
2024-02-12 19:18:31 +0100

    initial commit

---
+++ simple_calculator.py
@@ -0,0 +1,17 @@
+# Simple Calculator
+print("This is a Simple Calculator Script")
+num1 = float(input("First number: "))
+mathematical_operator = input("What mathematical operation do you want to take? [Only +, -, * and /] is allowed. \n")
+num2 = float(input("Second number: "))
+
+match mathematical_operator:
+    case "+":
+        print(num1 + num2)
+    case "-":
+        print(num1 - num2)
+    case "/":
+        print(num1 / num2)
+    case "*":
+        print(num1 * num2)
+    case _:
+        print("Invalid mathematical operator Input")
