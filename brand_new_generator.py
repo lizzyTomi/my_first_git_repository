ad8c89776a596c13e845fef6381fef7daf829f02
lawrence emmanuel <lizzytomi17@gmail.com>
2024-02-12 22:59:11 +0100

    inital commit

--- brand_new_generator.py
+++ brand_new_generator.py
@@ -3,3 +3,5 @@ city_n = input("please enter the name of your city here:\n")
 pet_n = input("Enter the name of your pet here\n")
 brand = (city_n + pet_n).title()
 print("Your brand name is  " + brand)
+
+
